553e6244c7ed08d9762c83f68d17e4e224218c67
Your Name <you@example.com>
2021-07-25 02:56:59 -0300

    .

--- faturamento/frontend/www/health.php
+++ faturamento/frontend/www/health.php
@@ -1,6 +1,8 @@
 <?php
 header('Content-Type: application/json');
 header("HTTP/1.1 200 OK");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
 
 $json['app'] = "ms-faturamento-frontend";
 $json['version'] = 1.0;
