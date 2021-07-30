afdc852eb914e8af4688fe6de544a6ffa032c79d
Your Name <you@example.com>
2021-07-30 10:20:43 -0300

    adjusting version

--- faturamento/frontend/www/version.php
+++ faturamento/frontend/www/version.php
@@ -1,6 +1,6 @@
 <?php
-$json_version['app'] = "ms-venda-frontend";
-$json_version['version'] = "1.0"    ;
+$json_version['app'] = "ms-faturamento-frontend";
+$json_version['version'] = "1.0";
 $json_version['status'] = true;
 $json_version['hostname'] = gethostname();
 ?>
